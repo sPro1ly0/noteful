3bc1c05d7e7ec02bf0bb24635a03d649227fe9c7
Sabrina Becker <sbecka8@gmail.com>
2020-01-10 14:45:39 -0500

    Create initial validation for folder name input

--- src/AddFolder.js
+++ src/AddFolder.js
@@ -1,19 +1,58 @@
 import React, { Component } from 'react';
 import './AddForms.css';
+import ValidationError from './ValidationError';
 
 class AddFolder extends Component {
 
-    handleSubmit(e) {
-        e.preventDefault();
+    constructor(props) {
+        super(props);
+        this.state = {
+            newFolder: {
+                value: '',
+                touched: false
+            }
+        }
+    }
+
+    updateNewFolder(newFolder) {
+        this.setState({
+            newFolder: {value: newFolder, touched: true}
+        });
+    }
+
+    validateFolderName(fieldValue) {
+        const name = this.state.newFolder.value;
+        if (name.length === 0) {
+            return 'A folder name is required';
+        } else if (name.length > 25) {
+            return 'Folder name must be no longer than 25 characters long.'
+        }
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const name = this.state.newFolder.value;
+        console.log(name);
 
     }
 
     render() {
         return (
-            <form className="add-folder-form">
-                <label htmlFor="new-folder">Enter New Folder Name:</label>
-                <input type="text" name="new-folder" id="new-folder" defaultValue=""/>
-                <button type="submit">+ Add New Folder</button>
+            <form className="add-folder-form" onSubmit={ e=> this.handleSubmit(e)}>
+                <label htmlFor="newFolder">Enter New Folder Name:</label>
+                <input 
+                    type="text" 
+                    name="newFolder" 
+                    id="newFolder" 
+                    defaultValue=""
+                    onChange={(e) => this.updateNewFolder(e.target.value)}/>
+                {this.state.newFolder.touched && (<ValidationError message={this.validateFolderName()}/>)}
+                <button 
+                    type="submit"
+                    disabled={
+                        this.validateFolderName()
+                    }
+                >+ Add New Folder</button>
             </form>
         )
     }

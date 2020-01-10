8f0b91fb21968b634dfca926e7a55c4ec401f47a
Sabrina Becker <sbecka8@gmail.com>
2020-01-10 16:32:32 -0500

    Add validation to AddNote form

--- src/AddNote.js
+++ src/AddNote.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './AddForms.css';
+import ValidationError from './ValidationError';
 import NotesContext from './NotesContext';
 
 class AddNote extends Component {
@@ -27,20 +28,45 @@ class AddNote extends Component {
     
     updateNewNote(newNote) {
         this.setState({
-            newNote: {value: newNote}
+            newNote: {value: newNote, touched: true}
         });
     }
     
     updateContent(content) {
         this.setState({
-            content: {value: content}
+            content: {value: content, touched: true}
         });
     }
 
     updateSelectedFolder(selectedFolder) {
         this.setState({
-            selectedFolder: {value: selectedFolder}
+            selectedFolder: {value: selectedFolder, touched: true}
         });
+    }
+
+    validateNoteName() {
+        const name = this.state.newNote.value;
+        if (name.length === 0) {
+            return 'Note name is required';
+        } else if (name.length > 30) {
+            return 'Folder name must be no longer than 30 characters long.'
+        }
+    }
+
+    validateContent() {
+        const content = this.state.content.value;
+        if (content.length === 0) {
+            return 'Content is required';
+        } else if (content.length > 1000) {
+            return 'Folder name must be no longer than 1000 characters long.'
+        }
+    }
+
+    validateSelectedFolder() {
+        const selectedFolder = this.state.selectedFolder.value;
+        if (selectedFolder === 'None') {
+            return 'Please select a folder.'
+        }
     }
 
     handleSubmit(event) {
@@ -49,6 +75,27 @@ class AddNote extends Component {
         console.log(newNote);
         console.log(content);
         console.log(selectedFolder);
+
+        fetch(`http://localhost:9090/notes`, {
+            method: 'POST',
+            body: JSON.stringify({name: `${newNote.value}`}), 
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error('Something went wrong, please try again later.');
+                }
+                return res.json()
+            })
+            .then(data => {
+                this.context.addNewFolder(data);
+                this.props.history.push('/');
+            })
+            .catch(error => {
+                this.setState({ error })
+            })
         
     }
 
@@ -67,6 +114,7 @@ class AddNote extends Component {
                         id="new-note" 
                         defaultValue=""
                         onChange={e => this.updateNewNote(e.target.value)}/>
+                    {this.state.newNote.touched && (<ValidationError message={this.validateNoteName()}/>)}
                 </div>
                 <div>
                     <label htmlFor="note-content">Content:</label>
@@ -76,6 +124,7 @@ class AddNote extends Component {
                         id="note-content" 
                         defaultValue="" 
                         onChange={e => this.updateContent(e.target.value)}/>
+                    {this.state.content.touched && (<ValidationError message={this.validateContent()}/>)}
                 </div>
                 <div>
                     <label htmlFor="note-folder">Folder:</label>
@@ -86,8 +135,15 @@ class AddNote extends Component {
                         <option value="None">Select Folder</option>
                         {folderOptions}
                     </select>
+                    {this.state.selectedFolder.touched && (<ValidationError message={this.validateSelectedFolder()}/>)}
                 </div>
-                <button type="submit">+ Add New note</button>
+                <button 
+                    type="submit"
+                    disabled={
+                        this.validateNoteName() ||
+                        this.validateContent() ||
+                        this.validateSelectedFolder()
+                    }>+ Add New note</button>
             </form>
         )
     }
